f32e4328b2ebbb927594e48b3a30ed2490cad16d
jhw <jjsu4852@naver.com>
2022-07-20 02:43:30 +0900

    0720-3

---
+++ script.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.mainText').animate({'opacity' : '1','margin-bottom' : '0px'},1000)
+    let mouse = function(){
+    $('.mouseWheel').animate({'top':'15px'},1000)
+    $('.mouseWheel').animate({'top':'5px'},1000)}
+    mouse()
+   setInterval(function(){
+    mouse()
+   },2000)
+})
